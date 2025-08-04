7aa8941aeca80e7a1a177d6a597069951081b325
Shirley Vitoncó <svitonco@yahoo.com>
2025-08-04 18:18:54 -0500

    This is my first commit

---
+++ Ejm_variables.py
@@ -0,0 +1,21 @@
+def calcular (a, b, operacion):
+    if operacion == '+':
+        return a + b
+    elif operacion == '-':
+        return a - b
+    elif operacion == '*':
+        return a * b
+    elif operacion == '/':
+        if b != 0:
+         return a/b
+        else:
+            return "error, no es posible dividir entre 0"
+    else:
+        return "valor ingresado no es válido"
+a = float(input("introduzca un numero: "))
+b = float(input("introduzca un numero: "))
+operacion = input("Introduzca el signo de la operacion deseada (+-*/)")
+
+resultado = calcular (a, b, operacion)
+print (f"Resultado de la operacion: {resultado}")
+
